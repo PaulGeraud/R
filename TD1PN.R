c667ce1252f3a1546a5edf335a033c3a249711b5
PaulGeraud <93342800+PaulGeraud@users.noreply.github.com>
2023-03-09 13:07:41 +0100

    Update TD1PN.R

--- TD1PN.R
+++ TD1PN.R
@@ -1,4 +1,4 @@
-rm(list=ls())#libere l'espace memoire des variables précedemment utilisées
+rm(list=ls())#libere l'espace memoire des variables prÃ©cedemment utilisÃ©es
 setwd("D:/R/datasets")
 install.packages("tidyverse") #Installation de la librairie tidyverse, on utilise  notamment les packages dplyr et ggplot2
 install.packages("ggforce")
@@ -18,30 +18,30 @@ library(matlib)
 library(mvtnorm)
 
 #Ex 1
-  drawMax=function(b,n,k) #b borne sup de la loi unif, n nombre d'échantillons, k nombre de tirages/ échantillon
+drawMax=function(b,n,k) #b borne sup de la loi unif, n nombre d'Ã©chantillons, k nombre de tirages/ Ã©chantillon
+{
+  A=rep(0,n)
+  for(i in seq(1,n))
   {
-    A=rep(0,n)
-    for(i in seq(1,n))
-    {
-      A[i]=max(runif(k,0,b))
-    }
-    return (A)
+    A[i]=max(runif(k,0,b))
   }
-  IC=function(esp,sd,n,alpha) #retourne l'intervalle de confiance sous l'hypothèse du TCL avec moyenne et variance connu pour un échantillon de taille n au risque alpha
-  {
-    A=seq(1,2)
-    z=-qnorm((alpha)/2)
-    A[0]=esp-z*(sd/sqrt(n))
-    A[1]=esp+z*(sd/sqrt(n))
-    return (A)
-  }
-  
+  return (A)
+}
+IC=function(esp,sd,n,alpha) #retourne l'intervalle de confiance sous l'hypothÃ¨se du TCL avec moyenne et variance connu pour un Ã©chantillon de taille n au risque alpha
+{
+  A=seq(1,2)
+  z=-qnorm((alpha)/2)
+  A[0]=esp-z*(sd/sqrt(n))
+  A[1]=esp+z*(sd/sqrt(n))
+  return (A)
+}
+
 MySample=drawMax(2,1000,100)
 moy=mean(MySample)
 MySd=sd(MySample)
 IntConf=IC(moy,MySd,1000,0.01)
 #Correction
-B=matrix(runif(100*1000,0,2),nrow=100,ncol=1000) #création d'une matrice de 100 lignes et 1000 colonnes Permet de stocker tous les tirages dans une matrice
+B=matrix(runif(100*1000,0,2),nrow=100,ncol=1000) #crÃ©ation d'une matrice de 100 lignes et 1000 colonnes Permet de stocker tous les tirages dans une matrice
 T=apply(B,2,max)#Applique la fonction max sur chaque colonne, pour le faire sur chaque ligne, remplacez le 2 par 1
 b=2
 n=100
@@ -60,17 +60,28 @@ for(i in 1:1000)
 sum(Tbar<IF[1])+sum(Tbar>IF[2]) #Permet de compter le nombre de valeurs en dehors de l'IF
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
 #Ex 2
-sdcor=rep(0,100)
-sdncor=rep(0,100)
-for(i in seq(100,10000,100))
+N=100
+Sn2=rep(0,100)
+Sn2Cor=rep(0,100)
+for(n in seq(100,10000,100))
 {
-  c=rnorm(i,mean = 0,sd=1)
-  sdcor[i/100]=sd(c)
-  sdncor[i/100]=((i-1)/i)*sd(c)
+  X=rnorm(n)
+  Sn2[n/100]=var(X)*(n-1)/n
+  Y=rnorm(n)
+  Sn2Cor[n/100]=var(Y)
 }
-plot(seq(1:100),rep(1,100),col="black",type="l",ylim=c(0.9,1.1),xlab="n*100",ylab="")
-points(seq(1:100),sdncor,col="red",cex=0.5)
-points(seq(1:100),sdcor,col="blue",cex=0.7)
+#correction + sous forme de lignes
+plot(seq(100,10000,100),Sn2,type="l",col="red")
+lines(seq(100,10000,100),Sn2Cor,col="blue")
+abline(a=1,b=0,col="black") #fait une ligne droite parallÃ¨le Ã  l'axe des abscisses
+#ma version sous forme de nuage de points
+plot(seq(1:100),rep(1,100),col="black",type="l",ylim=c(0.95,1.05),xlab="n*100",ylab="")
+points(seq(1:100),Sn2,col="red",cex=0.5)
+points(seq(1:100),Sn2Cor,col="blue",cex=0.7)
+
+#c'est mieux de faire 2 tirages diffÃ©rents un sera estimÃ© avec Sn2, l'autre avec Sn2Cor
+n=100
+
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
 #Ex 3
@@ -80,16 +91,29 @@ x=runif(N,-1,1)
 y=runif(N,-1,1)
 df<-data.frame(x,y)%>%
   mutate(norm=x^2+y^2)
-df%>%
-ggplot(aes(x,y))+geom_circle(aes(x0 = 0,y0=0,r=1))+geom_point(aes(colour=cut(norm,c(0,1,Inf))))+scale_color_manual(name="norm",values=c("(0,1]"="red",
-                                                                                                                                        "(1,Inf]"="blue"),
-                                                                                                                  labels=c("Inside circle","Outside circle"))
+df%>% #avec packages
+  ggplot(aes(x,y))+geom_circle(aes(x0 = 0,y0=0,r=1))+geom_point(aes(colour=cut(norm,c(0,1,Inf))))+scale_color_manual(name="norm",values=c("(0,1]"="red",
+                                                                                                                                          "(1,Inf]"="blue"),
+                                                                                                                     labels=c("Inside circle","Outside circle"))+
+  theme_classic()
+
+#sans package
+t=seq(-pi,pi,0.01)
+plot(cos(t),sin(t),type="l")
+
+couleur=ifelse(x^2+y^2<1,"red","blue")
+points(x,y,col=couleur)
+
+
+
+
+
 
 InCircle=sum(df$norm<1)
 #OutCircle=N-InCircle
 PiMC=4*InCircle/N
-#estimation hypersphère dimension 5
-N=10^6 #nombre de points N^5 tirages à réaliser
+#estimation hypersphÃ¨re dimension 5
+N=10^6 #nombre de points N^5 tirages Ã  rÃ©aliser
 a=runif(N,-1,1)
 b=runif(N,-1,1)
 c=runif(N,-1,1)
@@ -98,17 +122,17 @@ e=runif(N,-1,1)
 df2<-data.frame(a,b,c,d,e)%>%
   mutate(norm=a^2+b^2+c^2+d^2+e^2)
 InSphere5=sum(df2$norm<1)
-V5MC=32*InSphere5/N #valeur théorique 5.26379
+V5MC=32*InSphere5/N #valeur thÃ©orique 5.26379
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
 #Ex 4
-#Un estimateur de l'intégrale est 4x²y
+#Un estimateur de l'intÃ©grale est 4xÂ²y
 N=10000
 x=runif(N,0,2)
 y=runif(N,0,2)
 df3<-data.frame(x,y)%>%
   mutate(T=4*x^2*y)
-Est=mean(df3$T) #valeur réelle
-#graphes (non demandé)
+Est=mean(df3$T) #valeur rÃ©elle
+#graphes (non demandÃ©)
 x=seq(0,2,length.out=100)
 y=seq(0,2,length.out=100)
 myf=function(x,y)
@@ -118,7 +142,7 @@ myf=function(x,y)
 }
 #z2=matrix(outer(x,y,myf),nrow=100,ncol=100)
 #image(z2)
-z=outer(x,y,myf) #stock in a matrix val of z for (x,y) in [0;2]²
+z=outer(x,y,myf) #stock in a matrix val of z for (x,y) in [0;2]Â²
 colnames(z)=y  #change name of each column
 rownames(z)=x  #change name of each row
 df4=as.data.frame(z)%>% #convert the matrix into a data frame
@@ -126,7 +150,7 @@ df4=as.data.frame(z)%>% #convert the matrix into a data frame
   gather(y,value,-x)%>%
   mutate(y=as.numeric(y), 
          x=as.numeric(x),
-         value_range = cut(value, 20))#ligne intéressante
+         value_range = cut(value, 20))#ligne intÃ©ressante
 
 #2D
 df4%>%
@@ -149,20 +173,20 @@ rglwidget()
 #base graph
 persp3D(x,y,z)
 #add some legend
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image")
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image")
 #Change view angle. Theta control left right movements, phi up/down
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image"
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image"
         ,theta=45,phi=45)
 #Scale shafts
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image"
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image"
         ,theta=45,phi=45,
         color="purple",shade=0.4)
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image"
-#turn off box
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image"
+        #turn off box
         ,theta=45,phi=45,
         box=F)
 #turn on Axes (doesnt work ;(  )
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image"
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image"
         ,theta=45,phi=45,
         box=T,axes=T,nticks=10,ticktype='detailed')
 #create a color palette
@@ -172,7 +196,7 @@ nrow.z<-dim(z)[1]
 ncol.z<-dim(z)[2]
 zfacet<-z[-1,-1]+z[-1,-ncol.z]+z[-nrow.z,-1]+z[-nrow.z,-ncol.z]
 facet.color<-cut(zfacet,100)
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image"
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image"
         ,theta=45,phi=45,
         col=mycolors[facet.color],shade=0.9)
 #rgl package
@@ -191,14 +215,14 @@ with(quakes,
 plotrgl(lighting = TRUE, smooth = TRUE, cex=2)
 rglwidget()
 
-persp3D(x,y,z,main="3D Graph of z=x²y function on [0:2]² ",zlab="image"
+persp3D(x,y,z,main="3D Graph of z=xÂ²y function on [0:2]Â² ",zlab="image"
         ,theta=45,phi=45,
         box=T,axes=T,nticks=10,ticktype='detailed')
 
 persp3D(x,y,z,main="3D Graph of z=x^2y function on [0:2]x[0:2]",colvar=z,
         zlab="value of function",clab=c("Values","Numbers"),
         ticktype="detailed",nticks=1
-        )
+)
 plotrgl(lighting = TRUE, smooth = TRUE)
 
 #plot a gaussian vector
@@ -207,8 +231,8 @@ plotrgl(lighting = TRUE, smooth = TRUE)
 x=seq(-5,5,length.out=100)
 y=seq(-5,5,length.out=100)
 
-moy=c(0,0) #vecteur d'espérance
-sig=matrix(c(4,0,0,4),nrow=2) #matrice de covariance. Elle doit être positive,inversible et à déterminant positif
+moy=c(0,0) #vecteur d'espÃ©rance
+sig=matrix(c(4,0,0,4),nrow=2) #matrice de covariance. Elle doit Ãªtre positive,inversible et Ã  dÃ©terminant positif
 z=matrix(0,100,100)
 for(i in 1:100)
 {
@@ -223,15 +247,21 @@ plotrgl(lighting = TRUE, smooth = TRUE,fill=z)
 rglwidget()
 
 
- 
- Isig=inv(sig)
- GVect=function(x,y)
+
+
+
+
+
+Isig=inv(sig)
+GVect=function(x,y)
 {
-   a=c(x[0]-moy[0],y[0]-moy[1])
-   z=(1/sqrt(2*pi))^2*1/sqrt(sig)*exp(-(a%*%Isig%*%t(a))/2)
-   return (z)
- }
- VG=Vectorize(GVect)
+  a=c(x[0]-moy[0],y[0]-moy[1])
+  z=(1/sqrt(2*pi))^2*1/sqrt(sig)*exp(-(a%*%Isig%*%t(a))/2)
+  return (z)
+}
+VG=Vectorize(GVect)
+
+
 
  
 
