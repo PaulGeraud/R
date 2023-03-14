cc3c06f5a784ae09747bb5082501b907272c53ec
PaulGeraud <93342800+PaulGeraud@users.noreply.github.com>
2023-03-14 18:11:43 +0100

    Update TD2S.R

--- TD2S.R
+++ TD2S.R
@@ -2,6 +2,8 @@ rm(list=ls())
 install.packages("plot3D")
 install.packages("rgl")
 install.packages("plot3Drgl")
+
+
 setwd("D:/R/datasets")
 library(tidyverse)
 library(matlib)
@@ -13,6 +15,10 @@ library(plot3Drgl)
 mu0=6
 muE=7
 n=20
-sdE=2.4
-qt(0.95,df=19) #quantile de la loi de Student . df= degree of freedom
-pval=pt(1.81,df=19,lower.tail = FALSE) #probas de la loi de STudent
+sdE=2.4 #écart type non corrigé
+#partie1 (entreprise)
+qt(0.95,df=n-1) #quantile de la loi de Student . df= degree of freedom
+pval=pt(1.81,df=n-1,lower.tail = FALSE) #probas de la loi de STudent
+#partie2 (association)
+qt(0.05,df=n-1)
+pt(-1.81,df=n-1)
