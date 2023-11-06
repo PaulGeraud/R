e5ebad576ab1cdeed0dccadd5346ef71b941407e
Paul Géraud <93342800+PaulGeraud@users.noreply.github.com>
2023-11-06 14:16:11 +0100

    Update and rename ProjetStat.R to ProjetStatA2.R

--- ProjetStatA2.R
+++ ProjetStatA2.R
@@ -1,9 +1,9 @@
-#Préréglages
+#PrÃ©rÃ©glages
 
  
 library(tidyverse) #Installation de la librairie tidyverse, on utilise  notamment les packages dplyr et ggplot2
 library(VaRES) #La logCauchy & Pareto n'est pas fournie avec le package de base de R, on a besoin de cette libraire pour l'avoir. L'expression de la log cauchy est fausse
-setwd("D:/R/datasets") #définition du repository, à modifier selon l'installation
+setwd("D:/R/datasets") #dÃ©finition du repository, Ã  modifier selon l'installation
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
 #Importation du fichier , extraction des valeurs importantes et calcul de grandeurs statistiques
@@ -15,7 +15,7 @@ MyDataSet=Earthquakes%>%
   filter(YEAR>=1950,DAMAGE_MILLIONS_DOLLARS>0)%>%
   select(YEAR,DAMAGE_MILLIONS_DOLLARS)%>%
   arrange(DAMAGE_MILLIONS_DOLLARS)%>%
-  mutate(logDMG=log(DAMAGE_MILLIONS_DOLLARS))#Création du jeu exploité, on prend seulement les colonnes DAMAGE_MILLIONS_DOLLARS, year et I_D. Seulement les séismes après 1950 sont considérées
+  mutate(logDMG=log(DAMAGE_MILLIONS_DOLLARS))#CrÃ©ation du jeu exploitÃ©, on prend seulement les colonnes DAMAGE_MILLIONS_DOLLARS, year et I_D. Seulement les sÃ©ismes aprÃ¨s 1950 sont considÃ©rÃ©es
 MyDataSetSummarized=MyDataSet%>%
   summarize(Moyenne=mean(DAMAGE_MILLIONS_DOLLARS),
             Mediane=median(DAMAGE_MILLIONS_DOLLARS),
@@ -24,27 +24,27 @@ MyDataSetSummarized=MyDataSet%>%
             Variance=var(DAMAGE_MILLIONS_DOLLARS),
             EcartType=sd(DAMAGE_MILLIONS_DOLLARS),
             x0=log(median(MyDataSet$DAMAGE_MILLIONS_DOLLARS)),
-            gamma=(log(quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.75)/quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.25)))/2) #Calcul de la moyenne, médiane, 1er et 3ème quartile,variance et écart  et stockage dans un tab
+            gamma=(log(quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.75)/quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.25)))/2) #Calcul de la moyenne, mÃ©diane, 1er et 3Ã¨me quartile,variance et Ã©cart  et stockage dans un tab
 Eff2=read.csv("Eff2.csv", header = TRUE, sep = ";", quote = "\"", dec = ".",
               fill = TRUE, comment.char = "") #Pour des barplots
-plage=read.csv("plage.csv",sep=";") #Juste des valeurs allant de -10 à 10, c'est utilisé pour faire le graphique qui compare loi normale et loi de Cauchy
+plage=read.csv("plage.csv",sep=";") #Juste des valeurs allant de -10 Ã  10, c'est utilisÃ© pour faire le graphique qui compare loi normale et loi de Cauchy
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-#Calcul des paramètres de la Cauchy et log Cauchy, normale, exponentielle et Pareto
+#Calcul des paramÃ¨tres de la Cauchy et log Cauchy, normale, exponentielle et Pareto
 
-x0=median(MyDataSet$DAMAGE_MILLIONS_DOLLARS) #Paramètre de position de la Cauchy
-gamma=(quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.75)-quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.25))/2 #Paramètre d'échelle de la Cauchy
-logx0=log(median(MyDataSet$DAMAGE_MILLIONS_DOLLARS))#Paramètre de position de la logCauchy
-loggamma=(log(quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.75)/quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.25)))/2 #Paramètre d'échelle de la logCauchy
+x0=median(MyDataSet$DAMAGE_MILLIONS_DOLLARS) #ParamÃ¨tre de position de la Cauchy
+gamma=(quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.75)-quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.25))/2 #ParamÃ¨tre d'Ã©chelle de la Cauchy
+logx0=log(median(MyDataSet$DAMAGE_MILLIONS_DOLLARS))#ParamÃ¨tre de position de la logCauchy
+loggamma=(log(quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.75)/quantile(MyDataSet$DAMAGE_MILLIONS_DOLLARS,0.25)))/2 #ParamÃ¨tre d'Ã©chelle de la logCauchy
 moy=mean(MyDataSet$DAMAGE_MILLIONS_DOLLARS) #moyenne empirique
-sd=sd(MyDataSet$DAMAGE_MILLIONS_DOLLARS) #Ecart type non biaisé
-lambda=1/moy #Paramètre d'une loi exponentielle
+sd=sd(MyDataSet$DAMAGE_MILLIONS_DOLLARS) #Ecart type non biaisÃ©
+lambda=1/moy #ParamÃ¨tre d'une loi exponentielle
 xm=min(MyDataSet$DAMAGE_MILLIONS_DOLLARS) #Scale parameter Pareto
 theta=480/(sum(MyDataSet$logDMG)-480*xm) #Shape parameter Pareto
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-#Visualisation des différents tableaux
+#Visualisation des diffÃ©rents tableaux
 View(Earthquakes) #visualisation du tableau
-View(MyDataSet) #visualisation du jeu de données
-View(MyDataSetSummarized) #visualisation de la moyenne,médiane,quartiles, variance, écart type
+View(MyDataSet) #visualisation du jeu de donnÃ©es
+View(MyDataSetSummarized) #visualisation de la moyenne,mÃ©diane,quartiles, variance, Ã©cart type
 View(Eff2)
 View(plage)
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -52,26 +52,26 @@ View(plage)
 
 Eff2%>%
   ggplot(aes(x=DAMAGE.MillionUSD,fill=Model))+geom_bar(stat="count",alpha=1,position=position_dodge())+
-  theme_minimal()+labs(title="Modélisation des coûts matériels lié à un séisme",x="Dégats en Millions de dollars américains",y="Compteur",fill="Type de données")+
+  theme_minimal()+labs(title="ModÃ©lisation des coÃ»ts matÃ©riels liÃ© Ã  un sÃ©isme",x="DÃ©gats en Millions de dollars amÃ©ricains",y="Compteur",fill="Type de donnÃ©es")+
   scale_x_continuous(breaks=seq(0,1000, by =50))+
   scale_y_continuous(breaks=seq(0,450, by =50))+
-  scale_fill_discrete(labels=c("Exponentielle","LogCauchy","Mesuré","Pareto"))+
-  annotate(geom="text",x =1040, y=-10,label="Coût >=1000",color="black",size=3)
-  #Diagramme bâton
+  scale_fill_discrete(labels=c("Exponentielle","LogCauchy","MesurÃ©","Pareto"))+
+  annotate(geom="text",x =1040, y=-10,label="CoÃ»t >=1000",color="black",size=3)
+  #Diagramme bÃ¢ton
 Eff2%>%
   ggplot(aes(x=DAMAGE.MillionUSD))+
   stat_function(fun=dlogcauchy,n=100,args=list(mu=logx0,sigma=loggamma),colour="red")+annotate(geom="text",x =1000, y=0.0055,label="Log-Cauchy",color="red")+
   stat_function(fun=dexp,n=100,args=list(rate=lambda),colour="blue")+annotate(geom="text",x =1000, y=0.005,label="Exponentielle",color="blue")+
   stat_function(fun=dpareto,n=100,args=list(K=xm,c=theta),colour="green4")+annotate(geom="text",x =1000, y=0.0045,label="Pareto",color="green4")+
   theme_classic()+
-  labs(title="Fonction de densité de différentes distributions",x="x",y="y") #Représentation graphiques de densités,
+  labs(title="Fonction de densitÃ© de diffÃ©rentes distributions",x="x",y="y") #ReprÃ©sentation graphiques de densitÃ©s,
 Eff2%>%
   ggplot(aes(x=DAMAGE.MillionUSD))+
   stat_function(fun=plogcauchy,n=100,args=list(mu=logx0,sigma=loggamma),colour="red")+annotate(geom="text",x =1000, y=0.95,label="Log-Cauchy",color="red")+
   stat_function(fun=pexp,n=100,args=list(rate=lambda),colour="blue")+annotate(geom="text",x =1000, y=0.90,label="Exponentielle",color="blue")+
   stat_function(fun=ppareto,n=100,args=list(K=xm,c=theta),colour="green4")+annotate(geom="text",x =1000, y=0.85,label="Pareto",color="green4")+
   theme_classic()+
-  labs(title="Fonction de densité de différentes distributions",x="x",y="y") #Représentation graphiques des fonctions de répartitions,
+  labs(title="Fonction de densitÃ© de diffÃ©rentes distributions",x="x",y="y") #ReprÃ©sentation graphiques des fonctions de rÃ©partitions,
 
 plage%>%
   ggplot(aes(x=Plage))+stat_function(fun=dnorm,n=100)+stat_function(fun=dcauchy,n=100,colour="red")+
@@ -79,11 +79,11 @@ plage%>%
   annotate(geom="text",x =8.3, y=0.5,label="N(0,1)",color="black")+
   annotate(geom="text",x =9, y=0.45,label="Cauchy(0,1)",color="red")+
   labs(title="Comparaison graphique entre la loi de Cauchy(0,1) et la loi Normale(0,1)",x="x",y="y")
- #représentation graphique d'une densité d'une loi N(0,1) et d'une Cauchy(0,1)
+ #reprÃ©sentation graphique d'une densitÃ© d'une loi N(0,1) et d'une Cauchy(0,1)
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-#Calculs des effectifs théoriques qui seraient attendus si cela suivait une loi donnée (pour préparer le khi 2)
-#tests du khi2 degré de liberté 21-1-1-1=18 (car 2 paramètres estimés) 19 pour l'exponentielle car 1 seul paramètre
-#Les effectifs sont organisés par tranche de 50 MUSD, sauf la dernière tranche qui est >1000 MUSD
+#Calculs des effectifs thÃ©oriques qui seraient attendus si cela suivait une loi donnÃ©e (pour prÃ©parer le khi 2)
+#tests du khi2 degrÃ© de libertÃ© 21-1-1-1=18 (car 2 paramÃ¨tres estimÃ©s) 19 pour l'exponentielle car 1 seul paramÃ¨tre
+#Les effectifs sont organisÃ©s par tranche de 50 MUSD, sauf la derniÃ¨re tranche qui est >1000 MUSD
 
 #Suivant une Cauchy, ne marche pas
 effTCauchy=rep(0,21)
@@ -94,7 +94,7 @@ for(i in 1:20)
 }
 effTCauchy[21]=pcauchy(1000,location=x0,scale=gamma,lower.tail = TRUE)*480
 
-#Suivant une log Cauchy, je n'utilise pas la fonction du package VaRES car pour une raison que j'ignore, elle retourne des valeurs erronées pour le 21ème groupe
+#Suivant une log Cauchy, je n'utilise pas la fonction du package VaRES car pour une raison que j'ignore, elle retourne des valeurs erronÃ©es pour le 21Ã¨me groupe
 plogCauchy=function(x,u,s)
 {
   ret=0
@@ -141,19 +141,19 @@ for(i in 1:20)
 }
 effTExp[21]=(1-pexp(1000,lambda))*480
 
-#Affichage de ces effectifs théoriques
+#Affichage de ces effectifs thÃ©oriques
 print(effTCauchy)
 print(effTLogCauchy)
 print(effTNormale)
 print(effTExp)
 print(effTPareto)
 
-#Les effectifs observées dans le tableau, comptés à la main car je ne savais pas comment faire autrement
+#Les effectifs observÃ©es dans le tableau, comptÃ©s Ã  la main car je ne savais pas comment faire autrement
 effObs=c(298,39,14,10,10,10,3,4,4,6,6,1,0,3,2,3,1,1,0,2,63) 
 print(effObs)
-#chisq.test(effExp,p=effTCauchy) Je n'utilise pas cette méthode
+#chisq.test(effExp,p=effTCauchy) Je n'utilise pas cette mÃ©thode
 
-#Fonction fait main qui retourne le coeff du khi 2, t correspond aux effectifs théoriques, e aux effectifs mesurées
+#Fonction fait main qui retourne le coeff du khi 2, t correspond aux effectifs thÃ©oriques, e aux effectifs mesurÃ©es
 monchi2=function(t,e)
 {
   S=0
@@ -176,7 +176,7 @@ masomme=function(tab)
   return(S)
 }
 
-#Somme tous les effectifs, pour être sûr que il y a bien 480 valeurs
+#Somme tous les effectifs, pour Ãªtre sÃ»r que il y a bien 480 valeurs
 SEffExp=masomme(effExp)
 SEffCauchy=masomme(effTCauchy)
 SEffLogCauchy=masomme(effTLogCauchy)
